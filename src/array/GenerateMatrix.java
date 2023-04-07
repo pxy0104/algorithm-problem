fd4acc9609d654d157343d0f7a0aee632fae8bab
pxy <benlaixy@163.com>
2023-04-07 21:54:15 +0800

    螺旋矩阵

--- src/array/GenerateMatrix.java
+++ src/array/GenerateMatrix.java
@@ -1,10 +1,6 @@
 package array;
 
-/**
- * @author pxy
- * @software IntelliJ IDEA
- * @create 2023-04-07 21:27
- **/
+
 //给定一个正整数 n，生成一个包含 1 到 n^2 所有元素，
 // 且元素按顺时针顺序螺旋排列的正方形矩阵。
 public class GenerateMatrix {
